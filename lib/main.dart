157d77d4ed2eebbdbf216eabc491410009466095
Mennah Ayman <101893442+MennahAymann@users.noreply.github.com>
2022-08-10 05:39:12 +0200

    Project Completed

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,75 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.indigo.shade700,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
-                radius: 50,
+                radius: 70,
                 backgroundImage: AssetImage('images/me.jpeg'),
+              ),
+              Text(
+                'Omar Abdulaziz',
+                style: TextStyle(
+                  color: Colors.indigo.shade100,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Mouse Memoirs',
+                ),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  color: Colors.indigo.shade100,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Source Sans Pro',
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 15,
+                width: 150,
+                child: Divider(
+                  color: Colors.grey.shade400,
+                ),
+              ),
+              Card(
+                color: Colors.grey.shade400,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phonelink_ring,
+                    color: Colors.indigo.shade700,
+                  ),
+                  title: Text(
+                    '+2 106 772 7733',
+                    style: TextStyle(
+                      color: Colors.indigo.shade700,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.grey.shade400,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.indigo.shade700,
+                  ),
+                  title: Text(
+                    'omar.abdulaziz71@gmail.com',
+                    style: TextStyle(
+                      color: Colors.indigo.shade700,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
