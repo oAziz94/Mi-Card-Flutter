19b3375489537a6294a9c4c2d0374eda31a2a107
Mennah Ayman <101893442+MennahAymann@users.noreply.github.com>
2022-08-10 04:34:44 +0200

    First Commit

---
+++ android/app/src/main/kotlin/com/example/card_flutterr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.card_flutterr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo.shade700,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/me.jpeg'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
